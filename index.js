d1f87a1a25a1f54b6170a4142e19e7e55629afe4
nickavv <hello@nickvv.me>
2020-02-11 18:58:52 -0500

    structure client/server comms, start adding debate game client

--- index.js
+++ index.js
@@ -10,12 +10,39 @@ const port = process.env.PORT || 8524;
 // Mount our express HTTP router into our server
 server.on('request', app);
 
-// WebSocket Handling
+// Rooms is a map of room codes to a list of players in that room
+const rooms = new Map();
+
+// TODO adding a room manually until game-side exists
+rooms.set('test', []);
+
 const wss = new Server({ server });
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
-        console.log(message);
+        var radishMsg = JSON.parse(message);
+        if (radishMsg.messageType == 'JOINED_ROOM') {
+            if (!rooms.has(radishMsg.roomCode)) {
+                // Tried to connect to a non-existent room
+                const response = {
+                    messageType: 'ERROR_INVALID_ROOM',
+                    roomCode: radishMsg.roomCode,
+                    nickname: radishMsg.nickname
+                }
+                ws.send(JSON.stringify(response));
+            } else {
+                // Connected successfully
+                const players = rooms.get(radishMsg.roomCode);
+                players.push(radishMsg.nickname);
+                const response = {
+                    messageType: 'JOINED_ROOM_SUCCESS',
+                    roomCode: radishMsg.roomCode,
+                    nickname: radishMsg.nickname,
+                    vip: players.length == 1
+                }
+                ws.send(JSON.stringify(response));
+            }
+        }
     });
 });
 
-server.listen(port, () => console.log(`Free Radish Client is listening on port ${port}`));
+server.listen(port, () => console.log(`Free Radish is listening on port ${port}`));
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,43 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var socket = new WebSocket(location.origin.replace(/^http/, 'ws'));
 
-    document.getElementById('join-game-button').addEventListener('click', function() {
-        var roomCode = document.getElementById('form-roomcode').value;
-        var nickname = document.getElementById('form-name').value;
-        socket.send(`${nickname} joined room ${roomCode}`);
-        document.getElementById('room-code-form').classList.add('hidden');
+    var roomCode = '';
+    var nickname = '';
+    var vip = false;
+
+    document.querySelector('#join-game-button').addEventListener('click', function() {
+        document.querySelector('#form-roomcode-invalid').classList.add('hidden');
+        roomCode = document.querySelector('#form-roomcode').value;
+        nickname = document.querySelector('#form-name').value;
+        var joinedMsg = {
+            messageType: 'JOINED_ROOM',
+            roomCode,
+            nickname
+        };
+        socket.send(JSON.stringify(joinedMsg));
     });
+
+    socket.onmessage = (event) => {
+        var message = JSON.parse(event.data);
+        // Only respond to messages intended for us
+        if (message.roomCode == roomCode && message.nickname == nickname) {
+            switch(message.messageType) {
+            case 'ERROR_INVALID_ROOM':
+                document.querySelector('#form-roomcode-invalid').classList.remove('hidden');
+                break;
+            case 'JOINED_ROOM_SUCCESS':
+                vip = message.vip;
+                document.querySelector('#room-code-form').classList.add('hidden');
+                fetch('/debate.html')
+                    .then((response) => {
+                        return response.text();
+                    })
+                    .then((body) => {
+                        document.querySelector('#game-content').innerHTML = body;
+                        document.querySelector('#name-bar').innerHTML = nickname;
+                    });
+                break;
+            }
+        }
+    };
 });
