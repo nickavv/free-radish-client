b9e1567b3a511b57200bb8121ffe516e89efcc6f
nickavv <hello@nickvv.me>
2020-02-11 19:41:09 -0500

    handle players disconnecting

--- index.js
+++ index.js
@@ -14,35 +14,79 @@ server.on('request', app);
 const rooms = new Map();
 
 // TODO adding a room manually until game-side exists
-rooms.set('test', []);
+rooms.set('test', new Set());
 
 const wss = new Server({ server });
+// Used for connection liveness testing
+function noop() {};
+function heartbeat() {
+    this.isAlive = true;
+};
+function leaveRoom(ws) {
+    if (ws.inGame === true) {
+        // Remove the player from the game's lobby
+        const players = rooms.get(ws.room);
+        players.delete(ws.nick);
+        rooms.set(ws.room, players);
+    }
+}
+
+// Establish connections and handle events
 wss.on('connection', (ws) => {
+    ws.isAlive = true;
+    ws.room = '';
+    ws.nick = '';
+    ws.inGame = false;
+    ws.on('pong', heartbeat);
+
     ws.on('message', (message) => {
         var radishMsg = JSON.parse(message);
-        if (radishMsg.messageType == 'JOINED_ROOM') {
-            if (!rooms.has(radishMsg.roomCode.toLowerCase())) {
+        ws.room = radishMsg.roomCode.toLowerCase();
+        ws.nick = radishMsg.nickname.toLowerCase();
+        switch (radishMsg.messageType) {
+        case 'JOINED_ROOM': {
+            if (!rooms.has(ws.room)) {
                 // Tried to connect to a non-existent room
                 const response = {
                     messageType: 'ERROR_INVALID_ROOM',
-                    roomCode: radishMsg.roomCode,
-                    nickname: radishMsg.nickname
+                    roomCode: ws.room,
+                    nickname: ws.nick
                 }
                 ws.send(JSON.stringify(response));
             } else {
                 // Connected successfully
-                const players = rooms.get(radishMsg.roomCode.toLowerCase());
-                players.push(radishMsg.nickname);
+                ws.inGame = true;
+                const players = rooms.get(ws.room);
+                players.add(ws.nick);
+                rooms.set(ws.room, players);
                 const response = {
                     messageType: 'JOINED_ROOM_SUCCESS',
-                    roomCode: radishMsg.roomCode,
-                    nickname: radishMsg.nickname,
-                    vip: players.length == 1
+                    roomCode: ws.room,
+                    nickname: ws.nick,
+                    vip: players.size == 1
                 }
                 ws.send(JSON.stringify(response));
             }
         }
+        break;
+        case 'DISCONNECTED': {
+            leaveRoom(ws);
+        }
+        break;
+    }
     });
 });
+// Perform heartbeats to test for liveness
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) {
+        leaveRoom(ws);
+        return ws.terminate();
+    }
+
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 1000);
 
 server.listen(port, () => console.log(`Free Radish is listening on port ${port}`));
--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     document.querySelector('#join-game-button').addEventListener('click', function() {
         document.querySelector('#form-roomcode-invalid').classList.add('hidden');
-        roomCode = document.querySelector('#form-roomcode').value;
-        nickname = document.querySelector('#form-name').value;
+        roomCode = document.querySelector('#form-roomcode').value.toLowerCase();
+        nickname = document.querySelector('#form-name').value.toLowerCase();
         var joinedMsg = {
             messageType: 'JOINED_ROOM',
             roomCode,
@@ -19,8 +19,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     socket.onmessage = (event) => {
         var message = JSON.parse(event.data);
+        var room = message.roomCode.toLowerCase();
+        var nick = message.nickname.toLowerCase();
         // Only respond to messages intended for us
-        if (message.roomCode == roomCode && message.nickname == nickname) {
+        if (room == roomCode && nick == nickname) {
             switch(message.messageType) {
             case 'ERROR_INVALID_ROOM':
                 document.querySelector('#form-roomcode-invalid').classList.remove('hidden');
@@ -35,9 +37,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     .then((body) => {
                         document.querySelector('#game-content').innerHTML = body;
                         document.querySelector('#name-bar').innerHTML = nickname;
+                        if (vip == false) {
+                            document.querySelectorAll('.vip-only').forEach((item, i) => {
+                                item.classList.add('hidden');
+                            });
+                        }
                     });
                 break;
             }
         }
+    };
+
+    window.onbeforeunload = function() {
+        var disconnectedMsg = {
+            messageType: 'DISCONNECTED',
+            roomCode,
+            nickname
+        }
+        socket.send(JSON.stringify(disconnectedMsg));
+        socket.onclose = function () {}; // disable onclose handler first
+        socket.close();
     };
 });
